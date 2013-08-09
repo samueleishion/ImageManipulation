36669aa7ab9f6563d2962f85db1798e7f90f724d
Sam Acuna <samueleishion@gmail.com>
2013-08-08 21:41:54 -0500

    Documented Constants class so far.

--- src/Constants.java
+++ src/Constants.java
@@ -1,6 +1,18 @@
+/*
+ * Image Manipulation
+ * 	Constants Class
+ * 
+ * @author: Samuel Acuna
+ * @date: 08/2013
+ * 
+ * Constants class stores all the constants used in the 
+ * Image Manipulation project.    
+ * 
+ */
 
 public class Constants {
 	
+	// Digits that represent hexadecimal numbers
 	public  static final char[] DIGITS = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 
 	private Constants() { }
