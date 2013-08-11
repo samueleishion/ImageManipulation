171c6f9f037c6e71b279344faf9c81c9e6684971
Sam Acuna <samueleishion@gmail.com>
2013-08-11 15:36:45 -0500

    Added image class

---
+++ src/Image.java
@@ -0,0 +1,42 @@
+import javax.imageio.*; 
+import java.awt.image.BufferedImage; 
+import java.io.*; 
+
+public class Image {
+	
+	private String path; 
+	private BufferedImage img; 
+	private HexColor[][] colors; 
+	
+	public Image(String imgPathName) {
+		this.path = imgPathName; 
+		try {
+			File file = new File(imgPathName); 
+			this.img = ImageIO.read(file); 
+			this.processColors(); 
+		} catch(Exception e) {
+			e.printStackTrace(); 
+		}
+	}
+	
+	private void processColors() {
+		int width = this.img.getWidth(); 
+		int height = this.img.getHeight(); 
+		this.colors = new HexColor[height][width]; 
+		
+		for(int row = 0; row<height; row++) {
+			for(int col = 0; col<width; col++) {
+				int rgb = img.getRGB(col, row); 
+				int[] temp = new int[3]; 
+				temp[0] = (rgb >> 16) & 0xFF; // red
+				temp[1] = (rgb >>  8) & 0xFF; // green 
+				temp[2] = (rgb      ) & 0xFF; // blue
+				this.colors[row][col] = new HexColor(temp);  
+			}
+		}
+	}
+	
+	public String getImageName() { return this.path; }
+	public BufferedImage getImage() { return this.img; }
+	public HexColor[][] getColors() { return this.colors; }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 
 public class Test {
 
@@ -8,6 +10,8 @@ public class Test {
 	private static int TOLERANCE = 15;
 	
 	public static void main(String[] args) {
+		System.out.println("HEXADECIMAL"); 
+		System.out.println("-----------"); 
 		Hexadecimal hex = new Hexadecimal("ff"); 
 		System.out.println(hex);  
 		Hexadecimal hex2 = new Hexadecimal("1"); 
@@ -21,7 +25,8 @@ public class Test {
 			System.out.println(e); 
 		}
 		
-		System.out.println("\n");
+		System.out.println("\nHEXCOLOR");
+		System.out.println("--------"); 
 		
 		HexColor hc = new HexColor("9e0b0f"); 
 		HexColor hc2 = new HexColor("ee1c24"); 
@@ -29,6 +34,12 @@ public class Test {
 		System.out.println(hc2.getColor()); 
 		if(hc.isSimilar(hc2,TOLERANCE)) System.out.println("Similar colors"); 
 		else System.out.println("Different colors"); 
+		
+		System.out.println("\nIMAGE"); 
+		System.out.println("-----"); 
+
+		String uri = System.getProperty("user.dir")+File.separator+"iphone.png";
+		Image img = new Image(uri);  
 	}
 
 }
